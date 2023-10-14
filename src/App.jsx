86dab3bb36be0fc3a3e8824615b63a0802a3223e
Phannia <lorenae.silvas@gmail.com>
2023-10-13 20:02:06 -0400

    app.jsx modificado

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import { React, useState} from 'react'
 import Listado from './components/Listado'
 import Formulario from './components/Formulario'
+import { BaseColaboradores } from './assets/JS/BaseColaboradores.js'
+
+
 
 const App = () => {
+  const [ListaColaboradores, setListaColaboradores] = useState(BaseColaboradores);
   return (
     <main className='row'>
-        <section className='container '>
-            <Listado />
-        </section>
-        
         <section className='container'>
-            <Formulario />
+            <Listado 
+            ListaColaboradores={ListaColaboradores} 
+            setListaColaboradores={setListaColaboradores}
+            />
+        </section>
+        <section className='container'>
+            <Formulario 
+            ListaColaboradores={ListaColaboradores} 
+            setListaColaboradores={setListaColaboradores}
+            />
         </section>
         
     </main>
